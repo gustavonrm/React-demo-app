6abc8586c61dd5dcc233b4d6fce96cadbb47831f
gustavonrm <gustavo.magalhaes@jumpseller.com>
2024-06-13 20:27:46 +0100

    created comment

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -5,15 +5,16 @@ import { useCreateCommentMutation } from '../services/post.api';
 function CommentForm({postId}: {postId: any}) {
  
     const [createComment, { isLoading, isError, isSuccess}] = useCreateCommentMutation(); 
+    const [commentPayload, setCommentPayload ] = useState<{postId:any, name:string, body:string }>({postId: postId, name: '', body: ''})
 
     return (
         <div className="card mb-3">
         <div className="card-body">
             <h5 className="card-title">
-                <input className="form-control" type="text"  placeholder='type comment title'/>
+                <input className="form-control" type="text"  value={commentPayload.name} placeholder='type comment title' onChange={(e) => setCommentPayload({...commentPayload, name: e.target.value})}/>
             </h5>
-            <textarea className="form-control" rows={3}  placeholder='type comment body'/>
-            <button type="button" className="btn btn-success mt-3" onClick={() => createComment({ postId: postId, name: 'ola', body: 'body' })}>Submit</button>
+            <textarea className="form-control" rows={3} value={commentPayload.body} placeholder='type comment body'onChange={(e) => setCommentPayload({...commentPayload, body: e.target.value})}/>
+            <button type="button" className="btn btn-success mt-3" onClick={() => createComment(commentPayload)}>Submit</button>
             </div>        
         </div>
         )
